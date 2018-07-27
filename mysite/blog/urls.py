c06fe657f86eabb5608e5bba8d8a1ecdc15170d9
pranay-prince <pranayprincehbk@gmail.com>
2018-07-27 15:50:26 +0530

    Edit Page

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url 
 from . import views
 urlpatterns=[
-	url(r'^$',views.post_list, name='post_list'),
+    url('post/new/$', views.post_new, name='post_new'),
+	url(r'post/(?P<pk>[^/]+)/$', views.post_detail, name='post_detail'),
+    url(r'post/(?P<pk>[^/]+)/edit/$', views.post_edit, name='post_edit'),
+	url('$', views.post_list, name='post_list'),
 ]
